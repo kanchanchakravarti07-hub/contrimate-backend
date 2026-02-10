02a4a1a9f05a107ce1763a550c8dcb9d8881eab6
kanchanchakravarti07-hub <kanchanchakravarti07@gmail.com>
2026-02-10 17:44:47 +0530

    Fix New Chat refresh issue and Notification saving logic

--- src/main/java/com/contrimate/contrimate/entity/ExpenseSplit.java
+++ src/main/java/com/contrimate/contrimate/entity/ExpenseSplit.java
@@ -1,7 +1,7 @@
 package com.contrimate.contrimate.entity;
 
 import jakarta.persistence.*;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIgnore; // 🔥 Import Zaroori hai
 
 @Entity
 public class ExpenseSplit {
@@ -11,24 +11,32 @@ public class ExpenseSplit {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "expense_id")
-    @JsonIgnoreProperties("splits")
-    private Expense expense;
-
-    @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
 
-    private Double amountOwed;
+    @ManyToOne
+    @JoinColumn(name = "expense_id")
+    @JsonIgnore // 🔥 YE BAHUT ZAROORI HAI: Isse recursion rukega aur app crash nahi karegi
+    private Expense expense;
+
+    private Double amount;
+    
+    // Kabhi-kabhi frontend 'amountOwed' expect karta hai
+    private Double amountOwed; 
+
+    // --- Getters & Setters ---
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+
     public Expense getExpense() { return expense; }
     public void setExpense(Expense expense) { this.expense = expense; }
 
-    public User getUser() { return user; }
-    public void setUser(User user) { this.user = user; }
+    public Double getAmount() { return amount; }
+    public void setAmount(Double amount) { this.amount = amount; }
 
     public Double getAmountOwed() { return amountOwed; }
     public void setAmountOwed(Double amountOwed) { this.amountOwed = amountOwed; }
